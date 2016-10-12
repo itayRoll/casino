84090abb8152d86ed74cebb15b9dd7ae2d4b51c7
itayroll <itayroll1991@gmail.com>
2016-10-12 18:06:38 +0300

    opop

--- casino/utils.py
+++ casino/utils.py
@@ -1,14 +1,14 @@
-import requests
+# import requests
 from . import macros
 # def send_mail():
 # 	TODO
 
 
-def api_call(suffix, **params):
-	url = 'http://api.football-data.org/v1/' + suffix
-	headers = { 
-		'X-Auth-Token': 'd7034c57f4e44668b70e05f40765b961',
-		'X-Response-Control': 'minified'
-	}
-	response = requests.get(url, headers=headers, params=params)
-	return response
+# def api_call(suffix, **params):
+# 	url = 'http://api.football-data.org/v1/' + suffix
+# 	headers = { 
+# 		'X-Auth-Token': 'd7034c57f4e44668b70e05f40765b961',
+# 		'X-Response-Control': 'minified'
+# 	}
+# 	response = requests.get(url, headers=headers, params=params)
+# 	return response
